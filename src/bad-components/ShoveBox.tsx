238cc14d902357aebd5a4d2edb09f03de66adc19
Solenn Gacon <solgacon@udel.edu>
2023-03-15 11:10:28 -0400

    last commit for components task for ShoveBox!

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function ShoveBoxButton({
-    position,
-    setPosition
-}: {
-    position: number;
-    setPosition: (newPosition: number) => void;
-}) {
+interface previewProps {
+    currentPosition: number;
+}
+
+interface shoveBoxProps {
+    myMoveBox: () => void;
+}
+
+function ShoveBoxButton(props: shoveBoxProps) {
     return (
-        <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
+        //<Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
+        <Button onClick={props.myMoveBox}>Shove the Box</Button>
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox(props: previewProps): JSX.Element {
+    //const [position, setPosition] = useState<number>(10);
     return (
         <div
             data-testid="moveable-box"
@@ -25,26 +28,32 @@ function MoveableBox(): JSX.Element {
                 border: "1px solid blue",
                 display: "inline-block",
                 verticalAlign: "bottom",
-                marginLeft: position + "px"
+                //marginLeft: position + "px"
+                marginLeft: props.currentPosition + "px"
             }}
         ></div>
     );
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10);
+    const updatePosition = () => setPosition(4 + position);
+    //const box = MoveableBox();
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            {/*<span>The box is at: {box.position}</span>*/}
+            <span>The box is at: {position}</span>
             <div>
-                <ShoveBoxButton
+                {/*<ShoveBoxButton
                     position={box.position}
                     setPosition={box.setPosition}
                 ></ShoveBoxButton>
-                {box}
-            </div> */}
+    {box}*/}
+                <ShoveBoxButton myMoveBox={updatePosition}></ShoveBoxButton>
+                <MoveableBox currentPosition={position}></MoveableBox>
+            </div>
         </div>
     );
 }
