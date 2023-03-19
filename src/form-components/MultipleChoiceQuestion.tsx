c152634c5949854a72c7c61399f0ebd439b46b09
Solenn Gacon <solgacon@udel.edu>
2023-03-18 20:10:47 -0400

    App implementation looks good and tests passed for MultipleChoiceQuestion :) !

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,36 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    //This is State
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    //This is Control
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
+
+    function optionCorrect(): string {
+        if (choice === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
+
+    //This is View
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="listOfAnswers">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{optionCorrect()}</div>
         </div>
     );
 }
