4fa89383ac640b8a79951b9b9878d2d8dbdf7e8d
Solenn Gacon <solgacon@udel.edu>
2023-03-27 15:08:31 -0400

    tasks completely done!! Form task done with EditMode which finally worked!

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    //This is State
+    const [inEditMode, setInEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    //This is Control
+    function updateInEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setInEditMode(event.target.checked);
+    }
+
+    function updateUserName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
+
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
+    function textNotInEditMode(): string {
+        if (isStudent === true) {
+            return userName + " is a student";
+        } else {
+            return userName + " is not a student";
+        }
+    }
+
+    //This is View
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Switch
+                id="in-edit-mode"
+                label="In Edit Mode?"
+                checked={inEditMode}
+                onChange={updateInEditMode}
+            />
+            <div>
+                {inEditMode ? (
+                    <div>
+                        <Form.Check
+                            type="checkbox"
+                            id="is-student-check"
+                            label="Student?"
+                            checked={isStudent}
+                            onChange={updateIsStudent}
+                        />
+                        <Form.Group controlId="formStudentName">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control
+                                value={userName}
+                                onChange={updateUserName}
+                            />
+                        </Form.Group>
+                    </div>
+                ) : (
+                    textNotInEditMode()
+                )}
+            </div>
         </div>
     );
 }
